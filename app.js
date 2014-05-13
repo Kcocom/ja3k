550c2ffe59bd110cde781c4a01bbd3e3ecadb6eb
Calvin Chan <calvinwchan@me.com>
2014-05-12 20:07:47 -0700

    yelp api works

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//dependencies for each module used
+// dependencies for each module used
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -6,21 +6,42 @@ var handlebars = require('express3-handlebars');
 var graph = require('fbgraph');
 var passport = require('passport');
 var util = require('util');
-//load environment variables
+// load environment variables
 var dotenv = require('dotenv');
 dotenv.load();
 
 var app = express();
 
-//route files to load
+// route files to load
 var index = require('./routes/index');
 
-//fitbit api keys
+// fitbit api keys
 var CONSUMER_KEY = process.env.fitbit_consumer_key;
 var CONSUMER_SECRET = process.env.fitbit_consumer_secret;
 var AUTH_URL = process.env.fitbit_auth_URL;
 var ACCESS_TOKEN = "";
 var ACCESS_TOKEN_SECRET = "";
+
+// yelp api required info
+var YELP_CONSUMER_KEY = process.env.yelp_consumer_key;
+var YELP_CONSUMER_SECRET = process.env.yelp_consumer_secret;
+var YELP_TOKEN = process.env.yelp_token;
+var YELP_TOKEN_SECRET = process.env.yelp_token_secret;
+var YELP_ACCESS_TOKEN = "";
+var YELP_ACCESS_TOKEN_SECRET = "";
+
+var yelp = require("yelp").createClient({
+	consumer_key: YELP_CONSUMER_KEY, 
+	consumer_secret: YELP_CONSUMER_SECRET,
+	token: YELP_TOKEN,
+	token_secret: YELP_TOKEN_SECRET
+});
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+yelp.search({term: "food", location: "Montreal"}, function(error, data) {
+  console.log(error);
+  console.log(data);
+});
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
